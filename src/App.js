a41b1f981b08ce70561406a6146c91775e82296f
young sik seo <angelskying@gmail.com>
2019-12-13 16:48:25 +0900

    상품 api 추가

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import propTypes from 'prop-types';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
+  state = {
+    isLoading : false,
+    products : []
+  };
+
+
+  getProdList = async () => {
+    const { metadata } = await axios.get('https://api.bestbuy.com/beta/products/openBox(categoryId=abcat0400000)?apiKey=DQQ92FvXaeYdTJI9wPTUl0Lo');
+
+    console.log(metadata);
+  }
+
+  componentDidMount(){
+    this.getProdList();
+  }
+
+  render(){
+    const {isLoading} = this.state;
+
+    return (
+      <div>
+        Hi! {isLoading}
+      </div>
+    )
+  }
+};
 
 export default App;
