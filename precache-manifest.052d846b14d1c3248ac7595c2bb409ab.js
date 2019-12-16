efce55cdf14ded9a182817fef122d7d30daeecd8
seo <angelskying@gmail.com>
2019-12-16 10:36:59 +0900

    Updates

---
+++ precache-manifest.052d846b14d1c3248ac7595c2bb409ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f79b6ece0a9ef9a08d6f481e8bacc9",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "fe8bb17ac201659e9ba4",
+    "url": "/movie_app/static/js/2.9a6c68ef.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app/static/js/2.9a6c68ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c004ed02c07ea980a41",
+    "url": "/movie_app/static/js/main.d4fb8e2a.chunk.js"
+  },
+  {
+    "revision": "8cab6247e2673080e51b",
+    "url": "/movie_app/static/js/runtime-main.0b3ef422.js"
+  }
+]);
